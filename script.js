99e175ed48e2fab96868f5ded95cf28f453d13c6
BuildTools <alexander.schneider.2004@gmail.com>
2023-05-11 01:39:36 +0200

    Added more music

--- script.js
+++ script.js
@@ -3,22 +3,24 @@ var playBtn = document.getElementById("playBtn");
 var pauseBtn = document.getElementById("pauseBtn");
 var songTitle = document.getElementById("songTitle");
 var playlist = [
-  "HIRATHAE - Kentenshi.mp3",
-];
+    "HIRATHAE - Kentenshi.mp3",
+    "Ok Goodnight - Day & Night.mp3",
+    "Panic At The Disco! - I Write Sins Not Tragedies.mp3",
+    "Tokyopill - Club Cyberia.mp3",
+    "xaptiox - soup.mp3"
+  ];
 
 function playmusic() {
-  var randomIndex = Math.floor(Math.random() * playlist.length);
-  var randomSong = playlist[randomIndex];
-  audio.src = randomSong;
-  audio.play();
-  songTitle.textContent = "Now Playing: " + randomSong;
+    var audio = document.getElementById("myAudio");
+    var randomIndex = Math.floor(Math.random() * playlist.length);
+    var randomSong = playlist[randomIndex];
+    audio.src = randomSong;
+    audio.play();
+    songTitle.textContent = audio.src;
 }
 
 function stopmusic() {
-  audio.pause();
+    var audio = document.getElementById("myAudio");
+    audio.pause();
 }
-
-playBtn.addEventListener("click", playRandomSong);
-pauseBtn.addEventListener("click", stopmusic);
-
 
