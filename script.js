23f3a04b4a33bd148ca95400cfd26c437b8b66d2
BuildTools <alexander.schneider.2004@gmail.com>
2023-05-11 01:05:25 +0200

    I am dumb so I had to fix bg issues

--- script.js
+++ script.js
@@ -1,15 +1,24 @@
 var audio = document.getElementById("myAudio");
 var playBtn = document.getElementById("playBtn");
 var pauseBtn = document.getElementById("pauseBtn");
+var songTitle = document.getElementById("songTitle");
+var playlist = [
+  "HIRATHAE - Kentenshi.mp3",
+];
 
 function playmusic() {
-    var audio = document.getElementById("myAudio");
-    audio.play();
+  var randomIndex = Math.floor(Math.random() * playlist.length);
+  var randomSong = playlist[randomIndex];
+  audio.src = randomSong;
+  audio.play();
+  songTitle.textContent = "Now Playing: " + randomSong;
 }
 
 function stopmusic() {
-    var audio = document.getElementById("myAudio");
-    audio.pause();
-
+  audio.pause();
 }
+
+playBtn.addEventListener("click", playRandomSong);
+pauseBtn.addEventListener("click", stopmusic);
+
 
