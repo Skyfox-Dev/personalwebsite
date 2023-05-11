24f08c60f77ae40444a703bb9e434bd180b2587f
BuildTools <alexander.schneider.2004@gmail.com>
2023-05-11 21:02:00 +0200

    Fixed music playlist

--- script.js
+++ script.js
@@ -1,13 +1,10 @@
 var audio = document.getElementById("myAudio");
 var playBtn = document.getElementById("playBtn");
 var pauseBtn = document.getElementById("pauseBtn");
-var songTitle = document.getElementById("songTitle");
 var playlist = [
     "HIRATHAE - Kentenshi.mp3",
-    "Ok Goodnight - Day & Night.mp3",
-    "Panic At The Disco! - I Write Sins Not Tragedies.mp3",
     "Tokyopill - Club Cyberia.mp3",
-    "xaptiox - soup.mp3"
+    "xaptiox - soup.mp3",
   ];
 
 function playmusic() {
@@ -16,7 +13,6 @@ function playmusic() {
     var randomSong = playlist[randomIndex];
     audio.src = randomSong;
     audio.play();
-    songTitle.textContent = audio.src;
 }
 
 function stopmusic() {
