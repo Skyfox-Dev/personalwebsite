d35341a613f568b0f7cbd65527289eb24a5d7201
BuildTools <alexander.schneider.2004@gmail.com>
2023-05-14 23:07:04 +0200

    added a webradio instead of own files

--- script.js
+++ script.js
@@ -1,17 +1,9 @@
 var audio = document.getElementById("myAudio");
 var playBtn = document.getElementById("playBtn");
 var pauseBtn = document.getElementById("pauseBtn");
-var playlist = [
-    "HIRATHAE - Kentenshi.mp3",
-    "Tokyopill - Club Cyberia.mp3",
-    "xaptiox - soup.mp3",
-  ];
 
 function playmusic() {
     var audio = document.getElementById("myAudio");
-    var randomIndex = Math.floor(Math.random() * playlist.length);
-    var randomSong = playlist[randomIndex];
-    audio.src = randomSong;
     audio.play();
 }
 
